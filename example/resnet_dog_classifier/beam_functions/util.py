6bf677cdda9e548742de70708dbb69707168336c
iman <iman@syvlabs.com>
2019-07-25 11:58:06 +0800

    cleanup

--- example/resnet_dog_classifier/beam_functions/util.py
+++ example/resnet_dog_classifier/beam_functions/util.py
@@ -18,8 +18,8 @@ UNIQUE_ID = [ENTER YOUR NAME HERE]
 
 def run_command(shell_command, throw_error=True):
     try:
-    	logging.info(shell_command)
-    	return check_output(shell_command, shell=True, stderr=STDOUT)
+        logging.info(shell_command)
+        return check_output(shell_command, shell=True, stderr=STDOUT)
     except CalledProcessError as e:
         if throw_error:
             raise ValueError(e.output)
@@ -28,47 +28,47 @@ def run_command(shell_command, throw_error=True):
 
 
 def gcs_download_dir(remote_path, local_path='/tmp/'):
-	run_command('gsutil -m cp -r ' + remote_path + ' ' + local_path, throw_error=False)
+    run_command('gsutil -m cp -r ' + remote_path + ' ' + local_path, throw_error=False)
 
 
 def get_random_string(N):
-	return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(N))
+    return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(N))
 
 
 def create_table_schema():
-	table_schema = bigquery.TableSchema()
+    table_schema = bigquery.TableSchema()
 
-	url_schema = bigquery.TableFieldSchema()
-	url_schema.name = 'url'
-	url_schema.type = 'STRING'
-	url_schema.mode = 'NULLABLE'
-	table_schema.fields.append(url_schema)
+    url_schema = bigquery.TableFieldSchema()
+    url_schema.name = 'url'
+    url_schema.type = 'STRING'
+    url_schema.mode = 'NULLABLE'
+    table_schema.fields.append(url_schema)
 
-	breed_schema = bigquery.TableFieldSchema()
-	breed_schema.name = 'breed'
-	breed_schema.type = 'STRING'
-	breed_schema.mode = 'NULLABLE'
-	table_schema.fields.append(breed_schema)
+    breed_schema = bigquery.TableFieldSchema()
+    breed_schema.name = 'breed'
+    breed_schema.type = 'STRING'
+    breed_schema.mode = 'NULLABLE'
+    table_schema.fields.append(breed_schema)
 
-	prediction_schema = bigquery.TableFieldSchema()
-	prediction_schema.name = 'prediction'
-	prediction_schema.type = 'STRING'
-	prediction_schema.mode = 'NULLABLE'
-	table_schema.fields.append(prediction_schema)
+    prediction_schema = bigquery.TableFieldSchema()
+    prediction_schema.name = 'prediction'
+    prediction_schema.type = 'STRING'
+    prediction_schema.mode = 'NULLABLE'
+    table_schema.fields.append(prediction_schema)
 
-	score_schema = bigquery.TableFieldSchema()
-	score_schema.name = 'score'
-	score_schema.type = 'FLOAT64'
-	score_schema.mode = 'NULLABLE'
-	table_schema.fields.append(score_schema)
+    score_schema = bigquery.TableFieldSchema()
+    score_schema.name = 'score'
+    score_schema.type = 'FLOAT64'
+    score_schema.mode = 'NULLABLE'
+    table_schema.fields.append(score_schema)
 
-	return table_schema
+    return table_schema
 
 
 def create_pipeline_options():
     pipeline_options = PipelineOptions([
-    	'--project=tm-ml-platform',
-    	'--runner={}'.format(BEAM_RUNNER),
+        '--project=tm-ml-platform',
+        '--runner={}'.format(BEAM_RUNNER),
         '--temp_location=gs://ml-platform-dataflow-staging/temp',
         '--staging_location=gs://ml-platform-dataflow-staging/staging',
         '--job_name=doggos-predict-{}-{}'.format(UNIQUE_ID, get_random_string(6)),
@@ -76,7 +76,6 @@ def create_pipeline_options():
         '--region=us-west1',
         '--autoscaling_algorithm=NONE',
         '--worker_machine_type={}'.format(MACHINE_TYPE),
-        # '--requirements_file={}'.format(REQUIREMENTS_FILE),
         '--disk_size_gb=50',
         '--setup_file={}'.format(WORKER_SETUP_FILE)
     ])
